3e5014b8bf8b903ff87c9e72527f15929fd8e881
Wissididom <30803034+Wissididom@users.noreply.github.com>
2026-08-03 22:16:05 +0200

    Improve code with AI

--- api/index.php
+++ api/index.php
@@ -1,11 +1,13 @@
 <?php
-$validFor = 10; // Seconds
+ob_start();
+libxml_use_internal_errors(true);
+$validFor = 10; // seconds
 $_GET = array_change_key_case($_GET, CASE_LOWER);
 $dateFormat = 'd.m.Y';
 $useHtml = false;
 if (isset($_GET['dateformat']))
 	$dateFormat = $_GET['dateformat'];
-$timeFormat = 'H:i:Y';
+$timeFormat = 'H:i:s';
 if (isset($_GET['timeformat']))
 	$timeFormat = $_GET['timeformat'];
 $dateTimeFormat = 'd.m.Y H:i:s';
@@ -21,20 +23,26 @@ if (isset($_GET['fontfamily']) || isset($_GET['fontsize']) || isset($_GET['cssur
 if (isset($_GET['cssurl'])) {
 	$css_urls = explode(';', $_GET['cssurl']);
 	echo "\t<head>\n";
-	for ($i = 0; $i < count($css_urls); $i++) {
-		echo "\t\t<link rel=\"stylesheet\" href=\"" . $css_urls[$i] . "\">\n";
+	foreach ($css_urls as $css_url) {
+		echo "\t\t<link rel=\"stylesheet\" href=\"" . htmlspecialchars($css_url, ENT_QUOTES, 'UTF-8') . "\">\n";
 	}
 	echo "\t</head>\n";
 }
-$bodyTag = "\t<body style=\"";
-if (isset($_GET['fontfamily']))
-	$bodyTag .= 'font-family: ' . $_GET['fontfamily'] . ';';
-if (isset($_GET['fontsize']))
-	$bodyTag .= 'font-size: ' . $_GET['fontsize'] . ';';
-if (isset($_GET['fontcolor']))
-	$bodyTag .= 'color: ' . $_GET['fontcolor'] . ';';
-if ($useHtml === true)
-	echo $bodyTag . "\">\n";
+
+if ($useHtml) {
+	$styles = [];
+	if (isset($_GET['fontfamily']))
+		$styles[] = 'font-family: ' . htmlspecialchars($_GET['fontfamily'], ENT_QUOTES, 'UTF-8') . ';';
+	if (isset($_GET['fontsize']))
+		$styles[] = 'font-size: ' . htmlspecialchars($_GET['fontsize'], ENT_QUOTES, 'UTF-8') . ';';
+	if (isset($_GET['fontcolor']))
+		$styles[] = 'color: ' . htmlspecialchars($_GET['fontcolor'], ENT_QUOTES, 'UTF-8') . ';';
+	echo "\t<body";
+	if ($styles) {
+		echo ' style="' . implode(';', $styles) . '"';
+	}
+	echo ">\n";
+}
 
 function getWithCurl($url) {
     $ch = curl_init();
@@ -42,6 +50,8 @@ function getWithCurl($url) {
     curl_setopt($ch, CURLOPT_FAILONERROR, 1);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
     curl_setopt($ch, CURLOPT_TIMEOUT, 15);
     $retValue = curl_exec($ch);          
     curl_close($ch);
@@ -49,6 +59,10 @@ function getWithCurl($url) {
 }
 
 function xmlToArray($xml, $options = array()) {
+	if ($xml === false) {
+		http_response_code(502);
+		exit("Invalid XML returned by YouTube.");
+	}
     $defaults = array(
         'namespaceSeparator' => ':',//you may want this to be something other than a colon
         'attributePrefix' => '@',   //to distinguish between attributes and nodes with the same name
@@ -164,81 +178,63 @@ function linkify($value, $protocols = array('http', 'mail'), array $attributes =
 	// Insert all link
 	return preg_replace_callback('/<(\d+)>/', function ($match) use (&$links) { return $links[$match[1] - 1]; }, $value);
 }
-$index = 0;
-if (isset($_GET['channelid']) || isset($_GET['playlistid']) || isset($_GET['username'])) {
-	$xml = null;
-	$fileSeparator = '';
-	if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')           
-		$fileSeparator = '\\';
-	else 
-		$fileSeparator = '/';
-	if (!file_exists(getcwd() . $fileSeparator . 'cache'))
-		mkdir(getcwd() . $fileSeparator . 'cache');
-	if (isset($_GET['channelid'])) {
-		$channelid = $_GET['channelid'];
-		if (file_exists(getcwd() . $fileSeparator . 'cache' . $fileSeparator . $channelid . '.xml')) {
-			$valid_until = filemtime(getcwd() . $fileSeparator . 'cache' . $fileSeparator . $channelid . '.xml') + $validFor;
-			$expired = $valid_until < time();
-			if ($expired === true) {
-				$xml = getWithCurl('https://www.youtube.com/feeds/videos.xml?channel_id=' . $channelid);
-				file_put_contents(getcwd() . $fileSeparator . 'cache' . $fileSeparator . $channelid . '.xml', $xml);
-				$xml = xmlToArray(simplexml_load_string($xml));
-			} else {
-				$xml = xmlToArray(simplexml_load_string(file_get_contents(getcwd() . $fileSeparator . 'cache' . $fileSeparator . $channelid . '.xml')));
+function fetchFeed($cacheKey, $url, $validFor) {
+	$cacheDir = __DIR__ . DIRECTORY_SEPARATOR . 'cache';
+	$cacheFile = $cacheDir . DIRECTORY_SEPARATOR . $cacheKey . '.xml';
+	if (!file_exists($cacheDir))
+		mkdir($cacheDir, 0777, true);
+	if (file_exists($cacheFile)) {
+		$valid_until = filemtime($cacheFile) + $validFor;
+		$expired = $valid_until < time();
+		if ($expired) {
+			$xml = getWithCurl($url);
+			if ($xml === false) {
+				http_response_code(502);
+				exit("Unable to retrieve YouTube feed.");
 			}
-		} else {
-			$xml = getWithCurl('https://www.youtube.com/feeds/videos.xml?channel_id=' . $_GET['channelid']);
-			file_put_contents(getcwd() . $fileSeparator . 'cache' . $fileSeparator . $channelid . '.xml', $xml);
+			file_put_contents($cacheFile, $xml);
 			$xml = xmlToArray(simplexml_load_string($xml));
-		}
-	} else if (isset($_GET['playlistid'])) {
-		$playlistid = $_GET['playlistid'];
-		if (file_exists(getcwd() . $fileSeparator . 'cache' . $fileSeparator . $playlistid . '.xml')) {
-			$valid_until = filemtime(getcwd() . $fileSeparator . 'cache' . $fileSeparator . $playlistid . '.xml') + $validFor;
-			$expired = $valid_until < time();
-			if ($expired === true) {
-				$xml = getWithCurl('https://www.youtube.com/feeds/videos.xml?playlist_id=' . $playlistid);
-				file_put_contents(getcwd() . $fileSeparator . 'cache' . $fileSeparator . $playlistid . '.xml', $xml);
-				$xml = xmlToArray(simplexml_load_string($xml));
-			} else {
-				$xml = xmlToArray(simplexml_load_string(file_get_contents(getcwd() . $fileSeparator . 'cache' . $fileSeparator . $playlistid . '.xml')));
-			}
 		} else {
-			$xml = getWithCurl('https://www.youtube.com/feeds/videos.xml?playlist_id=' . $playlistid);
-			file_put_contents(getcwd() . $fileSeparator . 'cache' . $fileSeparator . $playlistid . '.xml', $xml);
-			$xml = xmlToArray(simplexml_load_string($xml));
-		}
-	} else if (isset($_GET['username'])) {
-		$username = $_GET['username'];
-		if (file_exists(getcwd() . $fileSeparator . 'cache' . $fileSeparator . $username . '.xml')) {
-			$valid_until = filemtime(getcwd() . $fileSeparator . 'cache' . $fileSeparator . $username . '.xml') + $validFor;
-			$expired = $valid_until < time();
-			if ($expired === true) {
-				$xml = getWithCurl('https://www.youtube.com/feeds/videos.xml?user=' . $username);
-				file_put_contents(getcwd() . $fileSeparator . 'cache' . $fileSeparator . $username . '.xml', $xml);
-				$xml = xmlToArray(simplexml_load_string($xml));
-			} else {
-				$xml = xmlToArray(simplexml_load_string(file_get_contents(getcwd() . $fileSeparator . 'cache' . $fileSeparator . $username . '.xml')));
-			}
-		} else {
-			$xml = getWithCurl('https://www.youtube.com/feeds/videos.xml?user=' . $username);
-			file_put_contents(getcwd() . $fileSeparator . 'cache' . $fileSeparator . $username . '.xml', $xml);
-			$xml = xmlToArray(simplexml_load_string($xml));
+			$xml = xmlToArray(simplexml_load_string(file_get_contents($cacheFile)));
 		}
 	} else {
-		echo "<!DOCTYPE html>\n<html>\n\t<body>\n\t\t<center style=\"color: red;\">\n\t\t\t500 Internal Server Error\n\t\t</center>\n\t</body>\n</html>\n";
-		http_response_code(500);
+		$xml = getWithCurl($url);
+		if ($xml === false) {
+			http_response_code(502);
+			exit("Unable to retrieve YouTube feed.");
+		}
+		file_put_contents($cacheFile, $xml);
+		$xml = xmlToArray(simplexml_load_string($xml));
+	}
+	return $xml;
+}
+$index = 0;
+if (isset($_GET['channelid']) || isset($_GET['playlistid'])) {
+	$xml = null;
+	if (isset($_GET['channelid'])) {
+		$channelid = $_GET['channelid'];
+		$xml = fetchFeed(hash('sha256', $channelid), 'https://www.youtube.com/feeds/videos.xml?channel_id=' . $channelid, $validFor);
+	} else if (isset($_GET['playlistid'])) {
+		$playlistid = $_GET['playlistid'];
+		$xml = fetchFeed(hash('sha256', $playlistid), 'https://www.youtube.com/feeds/videos.xml?playlist_id=' . $playlistid, $validFor);
+	} else {
+		http_response_code(400);
+		exit("Please either use channelid or playlistid.");
 	}
 	if (isset($_GET['index'])) {
-		$index = intval($_GET['index']);
+		$index = max(0, (int)$_GET['index']);
 	}
-	if ($xml != null && isset($_GET['redirect'])) {
-		header('Location: https://www.youtube.com/embed/' . $xml['feed']['entry'][$index]['yt:videoId'] . (isset($_GET['autoplay']) ? 'autoplay=1' : ''));
+	if (!isset($xml['feed']['entry'][$index])) {
+		http_response_code(404);
+		exit('Video index out of range.');
+	}
+	if (!empty($xml) && isset($_GET['redirect'])) {
+		header('Location: https://www.youtube.com/embed/' . $xml['feed']['entry'][$index]['yt:videoId'] . (isset($_GET['autoplay']) ? '?autoplay=1' : ''));
 		exit(0);
-	} else if ($xml != null && isset($_GET['redirecttothumbnail'])) {
+	} else if (!empty($xml) && isset($_GET['redirecttothumbnail'])) {
 		header('Location: ' . $xml['feed']['entry'][$index]['media:group']['media:thumbnail']['@url']);
 		exit(0);
-	} else if ($xml != null && isset($_GET['redirecttovideo'])) {
+	} else if (!empty($xml) && isset($_GET['redirecttovideo'])) {
 		header('Location: ' . $xml['feed']['entry'][$index]['link']['@href']);
 		exit(0);
 	} else if (isset($_GET['contenttype'])) {
@@ -248,52 +244,45 @@ if (isset($_GET['channelid']) || isset($_GET['playlistid']) || isset($_GET['user
 	} else {
 		header('Content-Type: text/plain');
 	}
-	if ($xml != null && isset($_GET['print'])) {
+	if (!empty($xml) && isset($_GET['print'])) {
 		$toPrint = $_GET['print'];
 		if (isset($_GET['contenttype']) || isset($_GET['fontfamily']) || isset($_GET['fontsize'])) {
 			$toPrint = "\t\t" . $toPrint;
 		}
 		// Date-Format-String-Description: https://www.php.net/manual/de/function.date.php
-		$toPrint = str_ireplace('$playlistid$', $xml['feed']['yt:playlistId'], $toPrint);
-		$toPrint = str_ireplace('$channelid$', $xml['feed']['yt:channelId'], $toPrint);
-		$toPrint = str_ireplace('$feedurl$', $xml['feed']['link']['@href'], $toPrint);
-		$toPrint = str_ireplace('$pltitle$', $xml['feed']['title'], $toPrint);
-		$toPrint = str_ireplace('$plauthorname$', $xml['feed']['author']['name'], $toPrint);
-		$toPrint = str_ireplace('$plauthorurl$', $xml['feed']['author']['uri'], $toPrint);
-		$toPrint = str_ireplace('$index$', $index, $toPrint);
-		$toPrint = str_ireplace('$videoid$', $xml['feed']['entry'][$index]['yt:videoId'], $toPrint);
-		$toPrint = str_ireplace('$uploaderid$', $xml['feed']['entry'][$index]['yt:channelId'], $toPrint);
-		if ($useHtml === true)
-			$toPrint = str_ireplace('$videotitle$', htmlentities($xml['feed']['entry'][$index]['media:group']['media:title']), $toPrint);
-		else
-			$toPrint = str_ireplace('$videotitle$', $xml['feed']['entry'][$index]['media:group']['media:title'], $toPrint);
-		$toPrint = str_ireplace('$videothumbnail$', $xml['feed']['entry'][$index]['media:group']['media:thumbnail']['@url'], $toPrint);
-		if ($useHtml === true)
-			$toPrint = str_ireplace('$videodescription$', htmlentities(linkify($xml['feed']['entry'][$index]['media:group']['media:description'], array('http', 'mail'), array('target' => '_blank'))), $toPrint);
-		else
-			$toPrint = str_ireplace('$videodescription$', $xml['feed']['entry'][$index]['media:group']['media:description'], $toPrint);
-		$toPrint = str_ireplace('$videostarcount$', $xml['feed']['entry'][$index]['media:group']['media:community']['media:starRating']['@count'], $toPrint);
-		$toPrint = str_ireplace('$videostaraverage$', $xml['feed']['entry'][$index]['media:group']['media:community']['media:starRating']['@average'], $toPrint);
-		$toPrint = str_ireplace('$videostarmin$', $xml['feed']['entry'][$index]['media:group']['media:community']['media:starRating']['@min'], $toPrint);
-		$toPrint = str_ireplace('$videostarmax$', $xml['feed']['entry'][$index]['media:group']['media:community']['media:starRating']['@max'], $toPrint);
-		$toPrint = str_ireplace('$videoviews$', $xml['feed']['entry'][$index]['media:group']['media:community']['media:statistics']['@views'], $toPrint);
-		$toPrint = str_ireplace('$videolink$', $xml['feed']['entry'][$index]['link']['@href'], $toPrint);
-		$toPrint = str_ireplace('$videoauthorname$', $xml['feed']['entry'][$index]['author']['name'], $toPrint);
-		$toPrint = str_ireplace('$videoauthorurl$', $xml['feed']['entry'][$index]['author']['uri'], $toPrint);
-		$date = new DateTime($xml['feed']['entry'][$index]['published']);
-		$toPrint = str_ireplace('$videopublished$', $date->format($dateFormat), $toPrint);
-		$toPrint = str_ireplace('$videopublishedtime$', $date->format($timeFormat), $toPrint);
-		$toPrint = str_ireplace('$videopublisheddatetime$', $date->format($dateTimeFormat), $toPrint);
-		$date = new DateTime($xml['feed']['entry'][$index]['updated']);
-		$toPrint = str_ireplace('$videoupdated$', $date->format($dateFormat), $toPrint);
-		$toPrint = str_ireplace('$videoupdatedtime$', $date->format($timeFormat), $toPrint);
-		$toPrint = str_ireplace('$videoupdateddatetime$', $date->format($dateTimeFormat), $toPrint);
-		$toPrint = str_replace("\\r", "\r", $toPrint);
-		$toPrint = str_replace("\\n", "\n", $toPrint);
-		if ($useHtml === true) {
-			$toPrint = str_ireplace("\n", '<br>', $toPrint);
-			$toPrint .= "\n";
-		}
+		$published = new DateTime($xml['feed']['entry'][$index]['published']);
+		$updated = new DateTime($xml['feed']['entry'][$index]['updated']);
+		$toPrint = strtr($toPrint, [
+			'$playlistid$' => $xml['feed']['yt:playlistId'],
+			'$channelid$' => $xml['feed']['yt:channelId'],
+			'$feedurl$' => $xml['feed']['link']['@href'],
+			'$pltitle$' => $xml['feed']['title'],
+			'$plauthorname$' => $xml['feed']['author']['name'],
+			'$plauthorurl$' => $xml['feed']['author']['uri'],
+			'$index$' => $index,
+			'$videoid$' => $xml['feed']['entry'][$index]['yt:videoId'],
+			'$uploaderid$' => $xml['feed']['entry'][$index]['yt:channelId'],
+			'$videotitle$' => $useHtml ? htmlspecialchars($xml['feed']['entry'][$index]['media:group']['media:title'], ENT_QUOTES, 'UTF-8') : $xml['feed']['entry'][$index]['media:group']['media:title'],
+			'$videothumbnail$' => $xml['feed']['entry'][$index]['media:group']['media:thumbnail']['@url'],
+			'$videodescription$' => $useHtml ? linkify(htmlspecialchars($xml['feed']['entry'][$index]['media:group']['media:description'], ENT_QUOTES, 'UTF-8'), array('http', 'mail'), array('target' => '_blank')) : $xml['feed']['entry'][$index]['media:group']['media:description'],
+			'$videostarcount$' => $xml['feed']['entry'][$index]['media:group']['media:community']['media:starRating']['@count'],
+			'$videostaraverage$' => $xml['feed']['entry'][$index]['media:group']['media:community']['media:starRating']['@average'],
+			'$videostarmin$' => $xml['feed']['entry'][$index]['media:group']['media:community']['media:starRating']['@min'],
+			'$videostarmax$' => $xml['feed']['entry'][$index]['media:group']['media:community']['media:starRating']['@max'],
+			'$videoviews$' => $xml['feed']['entry'][$index]['media:group']['media:community']['media:statistics']['@views'],
+			'$videolink$' => $xml['feed']['entry'][$index]['link']['@href'],
+			'$videoauthorname$' => $xml['feed']['entry'][$index]['author']['name'],
+			'$videoauthorurl$' => $xml['feed']['entry'][$index]['author']['uri'],
+			'$videopublished$' => $published->format($dateFormat),
+			'$videopublishedtime$' => $published->format($timeFormat),
+			'$videopublisheddatetime$' => $published->format($dateTimeFormat),
+			'$videoupdated$' => $updated->format($dateFormat),
+			'$videoupdatedtime$' => $updated->format($timeFormat),
+			'$videoupdateddatetime$' => $updated->format($dateTimeFormat),
+			"\\r" => "\r",
+			"\\n" => "\n",
+			"\n" => $useHtml ? "<br>" : "\n"
+		]);
 		echo $toPrint;
 	}
 } else {
@@ -304,10 +293,12 @@ if ($useHtml) {
 ?>
 	</body>
 </html>
-<?php } ?>
 <?php
+}
 /*if (isset($_GET['channelid'])) {
 	echo '<!-- <iframe width="560" height="315" src="' . $_SERVER['PHP_SELF'] . '?channelid=' . $_GET['channelid'] . '&redirect' . '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->';
 } else if (isset($_GET['playlistid'])) {
 	echo '<!-- <iframe width="560" height="315" src="' . $_SERVER['PHP_SELF'] . '?playlistid=' . $_GET['playlistid'] . '&redirect' . '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->';
-}*/ ?>
+}*/
+ob_end_flush();
+?>
